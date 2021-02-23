fecd68f9ce903884e1f9b8cfc977affc7e68e2f6
odz <odjhey@gmail.com>
2021-02-23 14:55:09 +0800

    refactor: extract server to own file

--- cmd/http-server/main.go
+++ cmd/http-server/main.go
@@ -1,22 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strings"
 )
-
-// PlayerStore ...
-type PlayerStore interface {
-	GetPlayerScore(name string) int
-	RecordWin(name string)
-}
-
-// PlayerServer ...
-type PlayerServer struct {
-	store PlayerStore
-}
 
 func main() {
 	server := &PlayerServer{NewInMemoryPlayerStore()}
@@ -24,28 +11,4 @@ func main() {
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen to 5000 %v", err)
 	}
-}
-
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	switch r.Method {
-	case http.MethodPost:
-		p.processWin(w, player)
-	case http.MethodGet:
-		p.showScore(w, player)
-	}
-}
-
-func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
-	score := p.store.GetPlayerScore(player)
-	if score == 0 {
-		w.WriteHeader(http.StatusNotFound)
-	}
-
-	fmt.Fprint(w, score)
-}
-
-func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
-	p.store.RecordWin(player)
-	w.WriteHeader(http.StatusAccepted)
 }
